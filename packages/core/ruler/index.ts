e2f2b610deb0482c27e02183ed44ba30fecbca34
123Fang <38906235+123Fang@users.noreply.github.com>
2024-06-26 08:30:18 +0800

    feat: initRuler函数，添加样式

---
+++ packages/core/ruler/index.ts
@@ -0,0 +1,8 @@
+import type { Canvas } from 'fabric/fabric-impl';
+import { fabric } from 'fabric';
+
+function initRuler(canvas: Canvas, options) {
+  console.log('test123')
+}
+
+export default initRuler;
