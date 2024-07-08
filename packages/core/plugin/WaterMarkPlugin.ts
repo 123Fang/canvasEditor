0ac9ce198645f38d80e0f6b2c76929defc0a4fc5
123Fang <38906235+123Fang@users.noreply.github.com>
2024-07-08 08:59:41 +0800

    fix: 水印插件

--- packages/core/plugin/WaterMarkPlugin.ts
+++ packages/core/plugin/WaterMarkPlugin.ts
@@ -10,11 +10,33 @@ enum POSITION {
   full = 'Full',
 }
 type IEditor = Editor;
+type IPosition = POSITION.lt | POSITION.lb | POSITION.rt | POSITION.rb | POSITION.full; // lt 左上 lr 左上 rt 右上  rb 右下 full 平铺 后续可扩展其他功能
+type IDrawOps = {
+  text: string;
+  size: number;
+  fontFamily: string;
+  color: string;
+  isRotate: boolean;
+  position: IPosition;
+};
+
+
+const defaultOptions: IDrawOps = {
+  text: '',
+  size: 24,
+  isRotate: false, // 是否倾斜
+  fontFamily: '汉体', // 可考虑自定义字体
+  color: '#ccc', // 可考虑自定义颜色
+  position: POSITION.lt,
+};
+
 
 
 class WaterMarkPlugin {
   public canvas: fabric.Canvas;
   public editor: IEditor;
+  private drawOps: IDrawOps = defaultOptions;
+  private hadDraw = false;
   constructor(canvas: fabric.Canvas, editor: IEditor) {
     this.canvas = canvas;
     this.editor = editor;
@@ -22,6 +44,17 @@ class WaterMarkPlugin {
   }
   init() {
     console.log('init-------init')
+    this.editor.on('sizeChange', this.drawWaterMark.bind(this));
+  }
+  drawWaterMark(ops: IDrawOps) {
+    this.drawOps = Object.assign(cloneDeep(this.drawOps), ops);
+    if (!this.drawOps.text) return;
+    const workspace = this.canvas.getObjects().find((item: any) => item.id === 'workspace');
+    const { width, height, left, top }: any = workspace;
+
+
+    
+    console.log(width, height, left, top)
   }
 };
 
